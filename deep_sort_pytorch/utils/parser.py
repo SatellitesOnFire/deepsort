a5cf1f5d57dce71ab21d05ad19782a55a946bf93
AlexBodner <61150961+AlexBodner@users.noreply.github.com>
2022-12-30 10:18:13 -0300

    Update parser.py

--- deep_sort_pytorch/utils/parser.py
+++ deep_sort_pytorch/utils/parser.py
@@ -34,8 +34,8 @@ def get_config(config_file=None):
 
 
 if __name__ == "__main__":
-    cfg = YamlParser(config_file=".../configs/yolov3.yaml")
-    cfg.merge_from_file(".../configs/deep_sort.yaml")
+    cfg = YamlParser(config_file="../configs/yolov3.yaml")
+    cfg.merge_from_file("../configs/deep_sort.yaml")
 
     import ipdb
     ipdb.set_trace()
